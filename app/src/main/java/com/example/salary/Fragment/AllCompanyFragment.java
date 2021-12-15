263b6a5b7eba9fc7088d7e238500834d5806dbeb
DESKTOP-ASQBEL4\hkh27 <gyounghan.ha@gmail.com>
2021-12-15 15:43:19 +0900

    리스트뷰 추가

---
+++ app/src/main/java/com/example/salary/Fragment/AllCompanyFragment.java
@@ -0,0 +1,43 @@
+package com.example.salary.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.salary.R;
+
+public class AllCompanyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activtiy_allcompany, container, false);
+
+        // 리스트 추가
+        final String[] company = {"한국예탁결제원", "한국수자원공사", "부산환경공단", "부산교통공사", "한국자산관리공사", "주택금융공사"};
+
+        ListView list = (ListView) rootView.findViewById(R.id.companyList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, company);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), company[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
+
+    }
+}
--- app/src/main/java/com/example/salary/MainActivity.java
+++ app/src/main/java/com/example/salary/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.salary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.example.salary.Fragment.AllCompanyFragment;
+import com.example.salary.Fragment.AreaCompanyFragment;
+import com.example.salary.Fragment.CenterCompanyFragment;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabs;
+    private Fragment allCompany = new AllCompanyFragment();
+    private Fragment centerCompany = new CenterCompanyFragment();
+    private Fragment areaCompany = new AreaCompanyFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // tabView
+        tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("전체"));
+        tabs.addTab(tabs.newTab().setText("중앙"));
+        tabs.addTab(tabs.newTab().setText("지방"));
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, allCompany).commit();
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = allCompany;
+                } else if (position == 1) {
+                    selected = centerCompany;
+                } else if (position == 2) {
+                    selected = areaCompany;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
 }
